d764986b605829f8d2be7e376f5baf6ad06b5baa
Snehita Kolli <snehitakolli20@gmail.com>
2022-05-01 23:12:49 -0700

    fixed sql files for the tables, connected to sql and initial question in index.js

--- db/schema.sql
+++ db/schema.sql
@@ -4,25 +4,31 @@ CREATE DATABASE hogwarts_db;
 USE hogwarts_db;
 
 CREATE TABLE department (
-    id INT AUTO_INCREMENT PRIMARY KEY, 
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL
-); 
+);
 
 CREATE TABLE role (
-    id INT AUTO_INCREMENT PRIMARY KEY, 
-    title VARCHAR(30) NOT NULL, 
-    salary DECIMAL (10,0), 
-    department_id INT, 
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(20, 2) NULL,
+    department_id INT NOT NULL,
     FOREIGN KEY (department_id)
-        REFERENCES department(id)
-        ON DELETE CASCADE ON UPDATE CASCADE,
-); 
+    REFERENCES department(id)
+    ON DELETE CASCADE ON UPDATE CASCADE
+);
 
 CREATE TABLE employee (
-    id INT AUTO_INCREMENT PRIMARY KEY, 
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
-    CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) 
-    manager_id INT NOT NULL,
+    manager_id INT NULL,
+    FOREIGN KEY (role_id)
+    REFERENCES role(id)
+    ON DELETE RESTRICT ON UPDATE CASCADE,
+    FOREIGN KEY (manager_id)
+    REFERENCES employee(id)
+    ON DELETE SET NULL 
+    ON UPDATE CASCADE
 );
---
+++ index.js
@@ -0,0 +1,79 @@
+require('dotenv').config();
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+const cTable = require('console.table');
+const figlet = require('figlet');
+
+// Create the connection to mySQL
+const db = mysql.createConnection(
+    {
+      host: 'localhost',
+      // MySQL username,
+      user: 'root',
+      // Add MySQL password here
+      password: 'password',
+      database: 'hogwarts_db'
+    },
+    console.log(`Connected to the hogwarts_db database.`)
+);
+
+// Connect to the database--> FIZ THIS LATE
+function startPrompt() {
+    const startQuestion = [{
+      type: "list",
+      name: "action",
+      message: "what would you like to do?",
+      loop: false,
+      choices: ["View all employees", "View all roles", "View all departments", "Add an employee", "Add a role", "Add a department", "Update role for an employee", "Update employee's manager", "View employees by manager", "Delete a department", "Delete a role", "Delete an employee", "View the total utilized budget of a department", "Quit"]
+    }]
+
+    inquirer.prompt(startQuestion)
+    .then(response => {
+        switch (response.action) {
+            case "View all employees":
+                viewAll("EMPLOYEE");
+            break;
+            case "View all roles":
+                viewAll("ROLE");
+            break;
+            case "View all departments":
+                viewAll("DEPARTMENT");
+            break;
+            case "Add a department":
+                addNewDepartment();
+            break;
+            case "Add a role":
+                addNewRole();
+            break;
+            case "Add an employee":
+                addNewEmployee();
+            break;
+            case "Update role for an employee":
+                updateRole();
+            break;
+            case "View employees by manager":
+                viewEmployeeByManager();
+            break;
+            case "Update employee's manager":
+                updateManager();
+            break;
+            case "Delete a department":
+                deleteDepartment();
+            break;
+            case "delete a role":
+                deleteRole();
+            break;
+              case "delete an employee":
+                deleteEmployee();
+                break;
+            case "View the total utilized budget of a department":
+                viewBudget();
+            break;
+            default:
+            connection.end();
+            }
+        })
+        .catch(err => {
+            console.error(err);
+    });
+}
